d509417519ca06b7658f08586e3a5ebefb09840d
gonzauru <inzaurraldegonzalo@gmail.com.com>
2018-11-10 20:58:08 -0300

    a ver

---
+++ WebApi/Controllers/HorarioController.cs
@@ -0,0 +1,25 @@
+﻿using WebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Logica;
+
+namespace WebApi.Controllers
+{
+    public class HorarioController : ApiController
+    {
+
+        public IHttpActionResult PostHorario(WebApi.Models.Horario horario)
+        {
+            Fachada fach = new Fachada();
+            long cedula = horario.cedula;
+            long idHorario = horario.fecha;
+            VOHorarioInsertarPaciente vo = new VOHorarioInsertarPaciente(idHorario, cedula);
+            fach.reservaProfesional(vo);
+            return Ok();
+        }
+    }
+}
---
+++ WebApi/Models/Horario.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApi.Models
+{
+    public class Horario
+    {
+        public long cedula { get; set; }
+        public long fecha { get; set; }
+    }
+}
