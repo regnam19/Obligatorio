267110b9314f07d477c13707844fd6b6d81ce020
gonzauru <inzaurraldegonzalo@gmail.com.com>
2018-11-18 23:17:21 -0300

    muestra horarios librea

---
+++ WebApi/mainhorarios.js
@@ -0,0 +1,55 @@
+﻿
+var columnDefs2 = [
+    { headerName: "Id Horario", field: "IdHorario", width: 150 },
+    { headerName: "Hora", field: "Hora", width: 150 },
+    { headerName: "Id Consultorio", field: "IdConsultorio", width: 200 }
+];
+
+var gridOptions2 = {
+    columnDefs: columnDefs2,
+    rowSelection: 'single',
+    onSelectionChanged: onSelectionChanged2
+};
+
+function obtenerIdProfesional() {
+    var selectedRows = gridOptions.api.getSelectedRows();
+    var selectedRowsString = '';
+    selectedRows.forEach(function (selectedRow, index) {
+        if (index !== 0) {
+            selectedRowsString += ', ';
+        }
+        selectedRowsString += selectedRow.cedula;
+    });
+    return selectedRowsString;
+};
+
+function onSelectionChanged2() {
+    var selectedRows = gridOptions2.api.getSelectedRows();
+    var selectedRowsString = '';
+    selectedRows.forEach(function (selectedRow, index) {
+        if (index !== 0) {
+            selectedRowsString += ', ';
+        }
+        selectedRowsString += selectedRow.nombre;
+    });
+    document.querySelector('#selectedRows').innerHTML = selectedRowsString;
+}
+
+
+function cargarHorarios() {
+    var gridDiv = document.querySelector('#myGridHorarios');
+    $('#myGridHorarios').empty();
+    new agGrid.Grid(gridDiv, gridOptions2);
+
+    $.getJSON('api/Horario/' + obtenerIdProfesional())
+        .done(function (data) {
+            $.each(data, function () {
+                gridOptions2.api.setRowData(data);
+            });
+        });
+
+
+};
+
+
+
