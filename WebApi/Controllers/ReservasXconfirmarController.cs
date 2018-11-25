4792a2aa05d64a431fffce704e41ddaa5179c125
Mateo Reyes <rchmateo@gmail.com>
2018-11-25 20:24:59 -0300

    preparar pacientehtml

--- WebApi/Controllers/ReservasXconfirmarController.cs
+++ WebApi/Controllers/ReservasXconfirmarController.cs
@@ -1,13 +1,37 @@
-﻿using System;
+﻿using Logica.VO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using WebApi.Models;
 
 namespace WebApi.Controllers
 {
     public class ReservasXconfirmarController : ApiController
     {
+        public IEnumerable<ReservasXconfirmar> GetReservasXconfirmar(long id)
+        {
+            Logica.Fachada fach = new Logica.Fachada();
+
+            ReservasXconfirmar[] reservas = new ReservasXconfirmar[fach.reservasXconfirmar(id).LongCount()];
+            int i = 0;
+            foreach(var r in fach.reservasXconfirmar(id))
+            {
+                reservas[i] = new ReservasXconfirmar();
+                reservas[i].IdReserva = r.IdReserva;
+                reservas[i].Dia = r.Dia;
+                reservas[i].Hora = r.Hora;
+                reservas[i].Consultorio = r.Consultorio;
+                reservas[i].NombrePaciente = r.NombrePaciente;
+                reservas[i].ApellidoPaciente = r.ApellidoPaciente;
+                i++;
+            }
+            
+            return reservas;
+        }
     }
 }
+
+
--- WebApi/Models/ReservasXconfirmar.cs
+++ WebApi/Models/ReservasXconfirmar.cs
@@ -7,5 +7,12 @@ namespace WebApi.Models
 {
     public class ReservasXconfirmar
     {
+        public long IdReserva { get; set; }
+        public int Hora { get; set; }
+        public DateTime Dia { get; set; }
+        public String Consultorio { get; set; }
+        public String NombrePaciente { get; set; }
+        public String ApellidoPaciente { get; set; }
+
     }
 }
