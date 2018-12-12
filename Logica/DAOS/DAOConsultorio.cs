bd8239b441651da9b19faa9d9850ecbae5c5b6e0
Mateo Reyes <rchmateo@gmail.com>
2018-12-11 21:28:25 -0300

    mateo

--- Logica/DAOS/DAOConsultorio.cs
+++ Logica/DAOS/DAOConsultorio.cs
@@ -176,5 +176,6 @@ namespace Logica
             return lista;
         }
 
+        
     }
 }
---
+++ Logica/VO/VOConsultorioReservado.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logica.VO
+{
+    public class VOConsultorioReservado
+    {
+        public DateTime Dia { get; set; }
+        public String Direccion { get; set; }
+        public int Hora { get; set; }
+        public long IdHorario { get; set; }
+
+        public VOConsultorioReservado() { }
+
+        public VOConsultorioReservado(DateTime dia, String direccion, int hora, long idhorario)
+        {
+            this.Dia = dia;
+            this.Direccion = direccion;
+            this.Hora = hora;
+            this.IdHorario = idhorario;
+        }
+    }
+}
