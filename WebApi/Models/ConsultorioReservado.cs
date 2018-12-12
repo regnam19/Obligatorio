12bc9db29bd7bc1b03d566d13ec487fae502f0c9
Mateo Reyes <rchmateo@gmail.com>
2018-12-11 21:42:04 -0300

    sube

---
+++ WebApi/Models/ConsultorioReservado.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApi.Models
+{
+    public class ConsultorioReservado
+    {
+        public DateTime Dia { get; set; }
+        public String Direccion { get; set; }
+        public int Hora { get; set; }
+        public long IdHorario { get; set; }
+    }
+}
